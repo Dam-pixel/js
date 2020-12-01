c5732d080a139a55bcea84cd5e7dc25dce19f495
Dam-pixel <73979404+Dam-pixel@users.noreply.github.com>
2020-12-01 21:26:12 +1300

    image lightbox done

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-document.querySelector ('.tab').addEventListener('click', function() {
-var panel = document.querySelector('.panel')
-var icon = document.querySelector('i')
-var text = document.querySelector('span')
-if(icon.classList.contains('fa-arrow-down')) {
-panel.style.height = "200px"
-icon.classList.remove('fa-arrow-down')
-icon.classList.add('fa-arrow-up')
-text.innerHTML = 'close'
-} else {
-    panel.style.height = "0"
-    icon.classList.replace('fa-arrow-up', 'fa-arrow-down')
-    text.innerHTML = 'open'
-}
+document.querySelectorAll('.slides img').forEach(function (slide) {
+    slide.addEventListener('click', function (image) {
+        console.log(image.target.src)
+        var source = image.target.src
+        document.querySelector('.popup').style.visibility = 'visible'
+        document.querySelector('.popup').style.opacity = 1
+        document.querySelector('.popup img').setAttribute('src', source)
+        //   document.querySelector('.popup img').style.visibility = "visible"
+        //   document.querySelector('.popup img').style.opacity = 1
+        document.querySelector('.popup img').style.cssText = 'visibility:visible;opacity:1;height:480px;width:640px;transform:rotate(360deg)'
+        document.querySelector('.close').style.display = 'block'
+    })
+
+})
+
+
+document.querySelector('.close').addEventListener('click', function(event) {
+
+    document.querySelector('.popup').style.cssText = 'visibility:hidden;opacity:0'
+    document.querySelector('.popup img').style.cssText = 'visibility:hidden;opacity:0'
+    event.target.style.display = 'none'
+
 })
